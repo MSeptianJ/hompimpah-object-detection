b776bf463d57fbc119cdb4a4038c3c2bcea1d782
Muhamad Septian Jaelani <mseptianjaelani@gmail.com>
2023-10-08 21:18:40 +0700

    Add Single Player Content

---
+++ src/routes/single/components/Choices.jsx
@@ -0,0 +1,53 @@
+import PropTypes from 'prop-types';
+import { PaperIlust, RockIlust, ScissorIlust } from '../../../Helper/ilusts';
+
+const Choices = ({ choice }) => {
+	const show = [
+		{
+			name: 'Rock',
+			ilust: <RockIlust />,
+		},
+		{
+			name: 'Paper',
+			ilust: <PaperIlust />,
+		},
+		{
+			name: 'Scissor',
+			ilust: <ScissorIlust />,
+		},
+	];
+	if (choice === 0) {
+		return (
+			<>
+				{show[0].ilust}
+				<p className=" text-lg font-bold">{show[0].name}</p>
+			</>
+		);
+	}
+
+	if (choice === 1) {
+		return (
+			<>
+				{show[1].ilust}
+				<p className=" text-lg font-bold">{show[1].name}</p>
+			</>
+		);
+	}
+
+	if (choice === 2) {
+		return (
+			<>
+				{show[2].ilust}
+				<p className=" text-lg font-bold">{show[2].name}</p>
+			</>
+		);
+	}
+
+	return null;
+};
+
+Choices.propTypes = {
+	choice: PropTypes.number,
+};
+
+export default Choices;
---
+++ src/routes/single/components/Scores.jsx
@@ -0,0 +1,52 @@
+import PropTypes from 'prop-types';
+import { StarFillIcon, StarOutlineIcon } from '../../../Helper/icons';
+
+const Scores = ({ score }) => {
+	if (score === 0) {
+		return (
+			<>
+				<StarOutlineIcon />
+				<StarOutlineIcon />
+				<StarOutlineIcon />
+			</>
+		);
+	}
+
+	if (score === 1) {
+		return (
+			<>
+				<StarFillIcon />
+				<StarOutlineIcon />
+				<StarOutlineIcon />
+			</>
+		);
+	}
+
+	if (score === 2) {
+		return (
+			<>
+				<StarFillIcon />
+				<StarFillIcon />
+				<StarOutlineIcon />
+			</>
+		);
+	}
+
+	if (score === 3) {
+		return (
+			<>
+				<StarFillIcon />
+				<StarFillIcon />
+				<StarFillIcon />
+			</>
+		);
+	}
+
+	return null;
+};
+
+Scores.propTypes = {
+	score: PropTypes.number,
+};
+
+export default Scores;
