345dfc447fe19ef50882d0e1c2ef77fdfc0ed6fc
Muhamad Septian Jaelani <mseptianjaelani@gmail.com>
2023-10-12 18:10:12 +0700

    Add Temporary React-Webcam Implementation

--- src/components/modalComponents/WebCamModal.jsx
+++ src/components/modalComponents/WebCamModal.jsx
@@ -1,20 +1,49 @@
 import { useAtom } from 'jotai';
+import { useCallback, useRef, useState } from 'react';
+import Webcam from 'react-webcam';
 import { webCamAtom } from '../../libs/atoms';
 import TitlePage from '../smallComponents/TitlePage';
 
 const WebCamModal = () => {
+	const FACING_MODE_USER = 'user';
+	const FACING_MODE_ENVIRONMENT = 'environment';
+	const videoConstraints = {
+		// width: 1280,
+		// height: 720,
+		facingMode: FACING_MODE_ENVIRONMENT,
+	};
+
+	const webCamRef = useRef(null);
+
 	const [cam, setCam] = useAtom(webCamAtom);
+
+	const [img, setImg] = useState(null);
+	const [facingMode, setFacingMode] = useState(FACING_MODE_USER);
+
+	const capture = useCallback(() => {
+		const imageSrc = webCamRef.current.getScreenshot();
+		setImg(imageSrc);
+	}, [webCamRef, setImg]);
+
+	const switchCamera = useCallback(() => {
+		setFacingMode((prevState) =>
+			prevState === FACING_MODE_USER
+				? FACING_MODE_ENVIRONMENT
+				: FACING_MODE_USER
+		);
+	}, []);
 
 	const handleBack = () => {
 		setCam(!cam);
 	};
 
 	const handleDetect = () => {
-		console.log('detecting');
+		capture();
 	};
 
 	const handleChangeCam = () => {
-		console.log('change Cam');
+		switchCamera();
+		console.log('Change camera to : ', facingMode);
 	};
 
 	return (
@@ -22,7 +51,35 @@ const WebCamModal = () => {
 			<TitlePage titleText="Hompimpah" />
 
 			<div className=" mx-auto w-full">
-				<div className=" mx-auto h-[30rem] max-h-[30rem] w-3/4 gap-4 rounded-md bg-slate-500 p-6 shadow-lg"></div>
+				<div className=" mx-auto flex h-[30rem] max-h-[30rem] w-3/4 flex-col justify-between gap-4 overflow-y-auto rounded-md bg-slate-500 p-6 shadow-lg">
+					<Webcam
+						ref={webCamRef}
+						audio={false}
+						height={1280}
+						screenshotFormat="image/jpeg"
+						width={720}
+						videoConstraints={{
+							...videoConstraints,
+							facingMode,
+						}}
+						className=" w-full rounded-md bg-slate-700"
+					/>
+
+					<div className=" w-full p-6">
+						{img && (
+							<div className=" mx-auto w-10/12">
+								<img src={img} className=" h-full w-full" />
+								<p>Result : Paper</p>
+								<button
+									onClick={handleBack}
+									className=" mx-auto w-1/2 bg-slate-600 p-2"
+								>
+									Accept
+								</button>
+							</div>
+						)}
+					</div>
+				</div>
 			</div>
 
 			<div className=" mx-auto flex w-full  max-w-md items-center justify-around text-center">
