5017248bde4c0e48bca8a5a4147df499e8a9801d
Muhamad Septian Jaelani <mseptianjaelani@gmail.com>
2023-10-15 13:39:24 +0700

    Change WebCam to Portrait

--- src/components/modalComponents/WebCamElement/WebCamDetect.jsx
+++ src/components/modalComponents/WebCamElement/WebCamDetect.jsx
@@ -10,49 +10,71 @@ const WebCamDetect = ({
 	isError,
 	isSuccess,
 }) => {
-	const [img] = useAtom(detImgAtom);
+	const videoConstraints = {
+		width: { min: 480 },
+		height: { min: 720 },
+		aspectRatio: 0.6666666667,
+	};
+
+	const [img, setImg] = useAtom(detImgAtom);
 	const [facingMode] = useAtom(camModeAtom);
 	const [detection] = useAtom(detDataAtom);
 
+	const handleRetryDetect = () => {
+		setImg(null);
+	};
+
 	return (
-		<div>
-			<div className=" w-full rounded-md bg-slate-700">
+		<>
+			{img ? (
+				<div className=" mx-auto w-full">
+					<div className=" w-full">
+						<img src={img} className=" h-full w-full" />
+					</div>
+
+					{isLoading && <p>Detecting Image...</p>}
+
+					{isError && <p>There is an Error</p>}
+
+					{isSuccess && (
+						<>
+							<p>{detection?.predictions[0]?.class || 'Nothing detected'}</p>
+							<p>
+								{`
+								Width : ${detection?.image?.width} Heigth :${detection?.image?.height}
+								`}
+							</p>
+						</>
+					)}
+
+					<div className=" grid w-full grid-cols-2 gap-4">
+						<button
+							onClick={handleBack}
+							className=" mx-auto w-full bg-slate-600 p-2"
+						>
+							Accept
+						</button>
+						<button
+							onClick={handleRetryDetect}
+							className=" mx-auto w-full bg-slate-600 p-2"
+						>
+							Retry
+						</button>
+					</div>
+				</div>
+			) : (
 				<Webcam
 					ref={camRef}
 					audio={false}
 					screenshotFormat="image/jpeg"
 					videoConstraints={{
+						...videoConstraints,
 						facingMode,
 					}}
-					className=" w-full rounded-md bg-slate-700"
+					className=" h-full w-full rounded-md bg-slate-700"
 				/>
-			</div>
-
-			<div className=" w-full p-6">
-				{img && (
-					<div className=" mx-auto w-10/12">
-						<div>
-							<img src={img} className=" h-full w-full" />
-						</div>
-
-						{isLoading && <p>Detecting Image...</p>}
-
-						{isError && <p>There is an Error</p>}
-
-						{isSuccess && (
-							<p>{detection?.predictions[0]?.class || 'Nothing detected'}</p>
-						)}
-
-						<button
-							onClick={handleBack}
-							className=" mx-auto w-1/2 bg-slate-600 p-2"
-						>
-							Accept
-						</button>
-					</div>
-				)}
-			</div>
-		</div>
+			)}
+		</>
 	);
 };
 
