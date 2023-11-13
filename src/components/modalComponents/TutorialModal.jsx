b482491550a4a71099c0c5ee16fd1dd17ef6b2b6
Muhamad Septian Jaelani <mseptianjaelani@gmail.com>
2023-11-13 16:15:16 +0700

    Add More Tutorial Content

--- src/components/modalComponents/TutorialModal.jsx
+++ src/components/modalComponents/TutorialModal.jsx
@@ -1,5 +1,4 @@
 import { useSetAtom } from 'jotai';
-import Ilust from '../../assets/img/KGB.svg';
 import { tutorModalAtom } from '../../libs/atoms';
 import { GoBackIcon } from '../../libs/icons';
 import BtnPrimary from '../smallComponents/BtnPrimary';
@@ -20,45 +19,44 @@ const TutorialModal = () => {
 				<div className=" mx-auto flex h-full w-3/4 flex-col gap-4 overflow-y-auto rounded-sm bg-slate-500 p-6 shadow-lg">
 					<section className=" w-full text-sm">
 						<h4 className=" mb-3 w-full text-left text-xl font-bold">
-							Description
-						</h4>
-						<p className=" mb-3 px-3 text-left">
-							Game Hompimpah ini menggunakan permainan Kertas Gunting Batu untuk
-							menentukan pemenang. Seperti umumnya, permainan Kertas Gunting
-							Batu menggunakan tiga bentuk tangan, seperti pada gambar berikut
-						</p>
-						<div className=" w-full rounded-md ">
-							<img
-								className=" mx-auto rounded-sm bg-slate-600 object-contain px-5 py-4"
-								src={Ilust}
-								alt="Ilustrasi Kertas Gunting Batu"
-							/>
-						</div>
-					</section>
-
-					<section className=" w-full text-sm">
-						<h4 className=" mb-3 w-full text-left text-xl font-bold">
 							How to Play
 						</h4>
-						<ul className=" w-full list-disc px-5 text-left">
-							<li>
+						<ul className=" w-full list-disc px-4 text-left">
+							<li className=" mb-2">
 								Untuk memainkan permainan ini, anda sebagai pemain akan
-								menggunakan kamera perangkat anda, untuk menampilkan bentuk
-								tangan yang anda pilih.
+								<b> menggunakan kamera perangkat anda</b>, untuk menampilkan
+								bentuk tangan yang anda pilih.
 							</li>
-							<li>
-								Sebelum memulai deteksi, pilih tempat yang memiliki cahaya yang
-								terang sehingga gambar yang ditampilkan web cam terlihat dengan
-								jelas.
+							<li className=" mb-2">
+								<b>Sebelum memulai deteksi</b>, pilih tempat yang memiliki
+								cahaya yang terang sehingga gambar yang ditampilkan web cam
+								terlihat dengan jelas.
 							</li>
-							<li>
-								Anda bisa bermain dengan camera depan (selfie) atau dengan
-								kamera belakang smartphone anda.
+							<li className=" mb-2">
+								Anda bisa bermain dengan <b>kamera depan (selfie)</b> atau
+								dengan <b>kamera belakang</b> smartphone anda.
 							</li>
-							<li>
-								Tekan tombol deteksi dan letakan tangan anda di depan kamera
-								dengan bentuk tangan yang anda pilih, diam kan selama proses
-								deteksi berlangsung
+							<li className=" mb-2">
+								Letakkan tangan anda di depan kamera dengan bentuk tangan yang
+								anda pilih, kemudian tekan tombol kamera untuk memulai proses
+								deteksi
+							</li>
+							<li className=" mb-2">
+								Setelah proses selesai, akan ditampilkan bounding box dan class
+								yang anda pilih, contohnya Kertas, Gunting, atau Batu
+							</li>
+							<li className=" mb-2">
+								<b>
+									Jika class yang ditampilkan sesuai dengan yang anda inginkan
+								</b>{' '}
+								tekan tombol centang berwarna hijau, tetapi jika tidak sesuai
+								anda dapat mengulangi proses deteksi dengan menekan tombol biru
+							</li>
+							<li className=" mb-2">
+								<b>Jika proses deteksi tidak menemukan apa-apa</b>, maka anda
+								harus memulai ulang deteksi. Hal ini terjadi jika gambar kurang
+								jelas, atau terlalu banyak warna pada gambar, sehingga
+								disarankan anda menggunakan background yang satu warna.
 							</li>
 						</ul>
 					</section>
@@ -69,7 +67,7 @@ const TutorialModal = () => {
 				<BtnPrimary
 					btnIcon={<GoBackIcon cls="w-full text-lg scale-150" />}
 					btnFunction={handle}
-					btnStyles="bg-slate-500 hover:bg-gray-700 text-slate-600"
+					btnStyles="bg-slate-500 hover:bg-gray-700 "
 				/>
 			</div>
 		</div>
