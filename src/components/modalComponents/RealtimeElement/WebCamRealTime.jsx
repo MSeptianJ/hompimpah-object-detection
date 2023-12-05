809227c82b8b1c7e31f02f4dda6b8469493a998a
Muhamad Septian Jaelani <mseptianjaelani@gmail.com>
2023-12-05 16:33:19 +0700

    Organize WebCamRealTime

--- src/components/modalComponents/RealtimeElement/WebCamRealTime.jsx
+++ src/components/modalComponents/RealtimeElement/WebCamRealTime.jsx
@@ -4,10 +4,8 @@ import { useCallback, useEffect, useRef, useState } from 'react';
 import Webcam from 'react-webcam';
 import LOADICON from '../../../assets/img/loading.svg';
 import {
-	camModeAtom,
 	detectDataAtom,
 	imgAccStateAtom,
-	isCamActiveAtom,
 	webCamModalAtom,
 } from '../../../libs/atoms';
 import { drawBoundingBox } from '../../../scripts/drawBoundingBox';
@@ -15,39 +13,32 @@ import wait from '../../../scripts/wait';
 import TitlePage from '../../smallComponents/TitlePage';
 
 const WebCamRealTime = () => {
-	const TIME = {
-		seconds: 10,
-		ms: 10000,
-	};
+	// Local State
 	const vites = import.meta.env;
-
-	// State
-	const [isCamReady, setIsCamReady] = useAtom(isCamActiveAtom);
+	const TIME = 5;
+	const TIMEMS = 5000;
+	const [isCamReady, setIsCamReady] = useState(false);
 	const [isCamDetecting, setIsCamDetecting] = useState(false);
-	const setImgAccState = useSetAtom(imgAccStateAtom);
-	const [timer, setTimer] = useState(TIME.seconds);
+	const [timer, setTimer] = useState(TIME);
 
-	// Camera
-	const [facingMode] = useAtom(camModeAtom);
+	// Atom State
+	const setImgAccState = useSetAtom(imgAccStateAtom);
+	const [detection, setDetection] = useAtom(detectDataAtom);
 
 	// Modal
 	const setCamModal = useSetAtom(webCamModalAtom);
-
-	// Detection
-	const [detection, setDetection] = useAtom(detectDataAtom);
 
 	// Ref
 	const webCamRef = useRef(null);
 	const canvasRef = useRef(null);
 
-	// Camera Config
+	// Camera
 	const checkCamActive = (camData) => {
-		console.count('change Camera State');
 		camData ? setIsCamReady(true) : setIsCamReady(false);
 	};
-
 	const videoConstraints = {
 		aspectRatio: 1,
+		facingMode: 'user',
 	};
 
 	const lastDetected = (detection) => {
@@ -69,8 +60,8 @@ const WebCamRealTime = () => {
 				publishable_key: vites.VITE_RBF_PUBLICKEY,
 			})
 			.load({
-				model: 'rockpaperscissors-t0zfu',
-				version: '10',
+				model: vites.VITE_RBF_MODEL,
+				version: vites.VITE_RBF_VERSION,
 			});
 
 		model.configure({
@@ -81,20 +72,23 @@ const WebCamRealTime = () => {
 		return model;
 	};
 
-	const detect = async (model) => {
+	const doDetection = async (model) => {
 		if (
 			typeof webCamRef.current !== 'undefined' &&
 			webCamRef.current !== null &&
 			webCamRef.current.video.readyState === 4
 		) {
 			const video = webCamRef.current.video;
+			const canvas = canvasRef.current;
+
 			const videoWidth = video.videoWidth;
 			const videoHeight = video.videoHeight;
 
-			webCamRef.current.video.width = videoWidth;
-			webCamRef.current.video.height = videoHeight;
-			canvasRef.current.width = videoWidth;
-			canvasRef.current.height = videoHeight;
+			video.width = videoWidth;
+			video.height = videoHeight;
+			canvas.width = videoWidth;
+			canvas.height = videoHeight;
+
 			await model.detect(video).then((prediction) => {
 				if (prediction?.length) {
 					setDetection(prediction);
@@ -107,21 +101,19 @@ const WebCamRealTime = () => {
 	const StartDetection = useCallback(async () => {
 		if (isCamReady) {
 			setIsCamDetecting(true);
+
 			const model = getModel();
 
-			// video.addEventListener('loadeddata', async () => {
 			model.then(async (model) => {
 				const detectingInterval = setInterval(() => {
-					detect(model);
+					doDetection(model);
 				}, 10);
 
-				await wait(TIME.ms);
+				await wait(TIMEMS);
 
 				clearInterval(detectingInterval);
 				model.teardown();
 			});
-
-			// });
 		}
 	}, [isCamReady]); // eslint-disable-line
 
@@ -131,13 +123,11 @@ const WebCamRealTime = () => {
 				setTimer((prev) => prev - 1);
 			}, 1000);
 
-			await wait(TIME.ms);
+			await wait(TIMEMS);
 
-			// Clear State
 			setIsCamDetecting(false);
 			setIsCamReady(false);
 			setCamModal(false);
-
 			setImgAccState(true);
 
 			clearInterval(timerInterval);
@@ -157,7 +147,7 @@ const WebCamRealTime = () => {
 				<div className="  mx-auto h-full w-full rounded-sm bg-slate-500 p-3 shadow-lg">
 					<div className="relative aspect-square h-full w-full">
 						{!isCamReady && (
-							<div className=" absolute top-0 w-full bg-yellow-600 text-sm opacity-50">
+							<div className=" absolute top-0 w-full bg-yellow-600 bg-opacity-50">
 								<p>Loading</p>
 								<img
 									src={LOADICON}
@@ -168,7 +158,7 @@ const WebCamRealTime = () => {
 						)}
 
 						{isCamDetecting && (
-							<div className=" absolute top-0 w-full bg-red-600 opacity-50">
+							<div className=" absolute top-0 w-full bg-red-600 bg-opacity-50">
 								<p>Show Your Hands</p>
 							</div>
 						)}
@@ -182,13 +172,10 @@ const WebCamRealTime = () => {
 							ref={webCamRef}
 							audio={false}
 							screenshotFormat="image/jpeg"
-							videoConstraints={{
-								...videoConstraints,
-								facingMode,
-							}}
-							muted
+							videoConstraints={videoConstraints}
 							onUserMedia={checkCamActive}
 							className=" h-full w-full rounded-sm bg-slate-700"
+							muted
 						/>
 					</div>
 				</div>
@@ -196,7 +183,7 @@ const WebCamRealTime = () => {
 
 			<div>
 				<p className=" text-center text-lg text-white">
-					{lastDetected(detection)}
+					Detected : {lastDetected(detection)}
 				</p>
 				<p className=" text-center text-lg text-white">{timer}</p>
 			</div>
