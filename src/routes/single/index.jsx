14bbbaf4e75f53c8c4f770e1502be9c213e98ec9
Muhamad Septian Jaelani <mseptianjaelani@gmail.com>
2023-12-13 20:39:01 +0700

    Change NoUser Element Style

--- src/routes/single/index.jsx
+++ src/routes/single/index.jsx
@@ -1,12 +1,12 @@
 import { useAtom } from 'jotai';
 import { useCallback, useEffect } from 'react';
+import BtnBackToMenu from '../../components/btnComponents/BtnBackToMenu';
 import BtnGameMenu from '../../components/btnComponents/BtnGameMenu';
 import BackModal from '../../components/modalComponents/BackModal';
 import GameResultModal from '../../components/modalComponents/GameResultModal';
 import RoundResultModal from '../../components/modalComponents/RoundResultModal';
 import TutorialModal from '../../components/modalComponents/TutorialModal';
 import WebCamModal from '../../components/modalComponents/WebCamModal';
-import NoUserBackBtn from '../../components/noUserComponents/NoUserBackBtn';
 import NoUserPlacement from '../../components/noUserComponents/NoUserPlacement';
 import TitlePage from '../../components/smallComponents/TitlePage';
 import {
@@ -134,7 +134,11 @@ const Single = () => {
 		<div className="grid max-h-screen min-h-screen w-full grid-rows-6 items-center text-center">
 			<TitlePage titleText="Round" accentText="1" />
 
-			<div className="z-0 row-span-4 mx-auto grid h-full w-2/3 grid-rows-2 items-center gap-5 overflow-y-auto rounded-[4px] text-backColor transition-all lg:h-auto lg:w-full lg:grid-cols-2 lg:grid-rows-1 ">
+			<div
+				className={`${
+					userUID ? '' : 'bg-primaryColor'
+				} z-0 row-span-4 mx-auto grid h-full w-2/3 grid-rows-2 items-center gap-5 overflow-y-auto rounded-[4px] text-backColor transition-all lg:h-auto lg:w-full lg:grid-cols-2 lg:grid-rows-1 `}
+			>
 				{userUID ? (
 					<SingleContent
 						P1Choise={P1Choise}
@@ -147,7 +151,7 @@ const Single = () => {
 				)}
 			</div>
 
-			{userUID ? <BtnGameMenu /> : <NoUserBackBtn />}
+			{userUID ? <BtnGameMenu /> : <BtnBackToMenu />}
 
 			{gameEndModal && <GameResultModal result={roundResult} />}
 			{roundEndModal && <RoundResultModal result={roundResult} />}
