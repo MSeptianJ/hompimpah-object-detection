ace987a9d79f9e5c412ce8c4b2c7fd7395f33741
Muhamad Septian Jaelani <mseptianjaelani@gmail.com>
2023-12-14 07:54:31 +0700

    Change Title Page Style

--- src/components/smallComponents/TitlePage.jsx
+++ src/components/smallComponents/TitlePage.jsx
@@ -2,15 +2,19 @@ import PropTypes from 'prop-types';
 
 const TitlePage = ({ titleText, accentText, isCol }) => {
 	return (
-		<div className="z-0 w-full text-center">
-			<h2
-				className={` flex items-center justify-center gap-3 text-3xl font-bold uppercase text-primaryColor ${
-					isCol ? 'flex-col' : ''
-				}`}
-			>
-				<p>{titleText}</p>
-				{accentText && <p className=" text-accentColor">{accentText || ''}</p>}
-			</h2>
+		<div className="z-0 m-auto">
+			<div className=" m-auto rounded-[4px] bg-primaryColor text-xl font-bold uppercase text-backColor shadow-lg shadow-[rgba(0,0,0,0.3)] ">
+				<div
+					className={`${
+						isCol ? 'flex-col' : ''
+					} flex w-full items-center justify-center gap-4 p-3 `}
+				>
+					<p>{titleText}</p>
+					{accentText && (
+						<p className=" text-accentColor">{accentText || ''}</p>
+					)}
+				</div>
+			</div>
 		</div>
 	);
 };
